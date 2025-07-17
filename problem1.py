e3be8f4967403969cc0b2efc938f110e02115138
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-17 22:08:44 +0500

    updated

--- problem1.py
+++ problem1.py
@@ -1,17 +1,31 @@
 
-def locate_card(cards,query):
-    position = 0
+# def locate_card(cards,query):
+#     position = 0
 
-    while True:
-        if cards[position] == query:
-            return position
-        position += 1
+#     while True:
+#         if cards[position] == query:
+#             return position
+#         position += 1
 
-        if position == len(cards):
-            return -1
+#         if position == len(cards):
+#             return -1
         
 
 
-cards = [78,34,23,11,8]
-query = 23
-print(locate_card(cards,query))  
+# cards = [78,34,23,11,8]
+# query = 23
+# print(locate_card(cards,query))  
+
+
+# def locate_card(cards, query):
+#     position = 0
+
+#     while position < len(cards):
+#         if cards[position] == query:
+#             return position
+#         position += 1
+
+#     return -1
+# cards = []
+# query = 0
+# print(locate_card(cards,query))

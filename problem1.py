5a0defd0b5f6dd498036f482583727a2890fa5e1
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-17 22:10:09 +0500

    updated version

--- problem1.py
+++ problem1.py
@@ -29,3 +29,22 @@
 # cards = []
 # query = 0
 # print(locate_card(cards,query))
+
+
+def locate_card(cards, query):
+    position = 0
+
+    while position < len(cards):
+        if cards[position] == query:
+            print(f" Found at position {position}")
+            return position
+        position += 1
+
+    print("Number does not exist in the list.")
+    return -1
+
+
+cards = [78, 34, 23, 11, 8]
+query = 23
+
+position = locate_card(cards, query)

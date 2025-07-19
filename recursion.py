0c428183b228e39fe550d1130a9956b8ad50e77d
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-19 12:26:40 +0500

    updated

--- recursion.py
+++ recursion.py
@@ -1,8 +1,18 @@
-def countDown(n):
-    if n == 0:
-        return "Blast off!"
-    else:
-        print(n)
-        return countDown(n-1)
+# def countDown(n):
+#     if n == 0:
+#         return "Blast off!"
+#     else:
+#         print(n)
+#         return countDown(n-1)
     
-countDown(6)
+# countDown(6)
+
+
+def countUp(current,n):
+    if current > n:
+        return "reached"
+    else:
+        print(current)
+        return countUp(current+1,n)
+
+countUp(1,6)

420b17b5c501b61f00cba8aaee1cc996f62d3a53
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-19 12:33:33 +0500

    updated

--- recursion.py
+++ recursion.py
@@ -8,11 +8,36 @@
 # countDown(6)
 
 
-def countUp(current,n):
-    if current > n:
-        return "reached"
-    else:
-        print(current)
-        return countUp(current+1,n)
+# def countUp(current,n):
+#     if current > n:
+#         return "reached"
+#     else:
+#         print(current)
+#         return countUp(current+1,n)
 
-countUp(1,6)
+# countUp(1,6)
+
+
+# sum of n natural numbers
+
+# def sum(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n + sum(n-1)
+    
+# print(sum(5))
+
+
+# factorail
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+print(factorial(4))
+
+    
+    

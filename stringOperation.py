bebfaff56ca978d3a988ba149944874fefd2ee60
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-21 00:16:18 +0500

    updated

---
+++ stringOperation.py
@@ -0,0 +1,15 @@
+# print(len(s))       # Length of the string
+# print(s[0])         # First character
+# print(s[-1])        # Last character
+# print(s[1:4])       # Substring from index 1 to 3 => "azh"
+# print(s.upper())    # "MAZHAR"
+# print(s.lower())    # "mazhar"
+# print(s[::-1])      # Reverse string => "rahzam"
+# print("z" in s)     # True
+# print(s.replace("a", "x"))
+
+
+def length(s):
+    return len(s)
+
+print(length("MAZHAR"))

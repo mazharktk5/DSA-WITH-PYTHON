da30d3f80d672e36443b3939fe638471bd3d01a6
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-23 20:05:53 +0500

    updated

--- twoPointers.py
+++ twoPointers.py
@@ -13,20 +13,20 @@ def has_pair_with_sum(arr, target):
     return False
 
 
-arr = [1, 3, 4, 5, 7, 10, 12]
-print(has_pair_with_sum(arr, 20))
+# arr = [1, 3, 4, 5, 7, 10, 12]
+# print(has_pair_with_sum(arr, 20))
 
-arr = [2, 4, 6, 8, 9, 11, 15]
-target = 17
+# arr = [2, 4, 6, 8, 9, 11, 15]
+# target = 17
 
-Loop 1: arr[left] + arr[right] = 13
-curr_sum = 13 → move right
+# Loop 1: arr[left] + arr[right] = 13
+# curr_sum = 13 → move right
 
-Loop 2: arr[left] + arr[right] = 15
-curr_sum = 15 → move right
+# Loop 2: arr[left] + arr[right] = 15
+# curr_sum = 15 → move right
 
-Loop 3: arr[left] + arr[right] = 16
-curr_sum = 16 → move right
+# Loop 3: arr[left] + arr[right] = 16
+# curr_sum = 16 → move right
 
-Loop 4: arr[left] + arr[right] = 17
-curr_sum = 17 → return true
+# Loop 4: arr[left] + arr[right] = 17
+# curr_sum = 17 → return true

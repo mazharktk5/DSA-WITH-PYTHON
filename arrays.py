2fd4a6e67310cf5147449b30a217b512aa62ceaf
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-23 09:43:01 +0500

    updated

--- arrays.py
+++ arrays.py
@@ -1,18 +1,18 @@
 
 
-# list = [1,3,5,7,8]
+list = [1,3,5,7,8]
 
-# list.insert(0,2)
-# print(list)
-# list.insert(2,9)
-# print(list)
-# list.insert(len(list),10)
-# print(list)
+list.insert(0,2)
+print(list)
+list.insert(2,9)
+print(list)
+list.insert(len(list),10)
+print(list)
 
-# list.remove(8)
-# print(list)
-# del list[0]
-# print(list)
+list.remove(8)
+print(list)
+del list[0]
+print(list)
 
-# for i in range(len(list)):
-#     print(list[i])
+for i in range(len(list)):
+    print(list[i])

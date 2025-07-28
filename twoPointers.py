6fb446cf1d26d29801d0336904a9db8029aedb6e
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-28 18:21:39 +0500

    updated

--- twoPointers.py
+++ twoPointers.py
@@ -1,16 +1,16 @@
-# def has_pair_with_sum(arr, target):
-#     left = 0
-#     right = len(arr) - 1
+def has_pair_with_sum(arr, target):
+    left = 0
+    right = len(arr) - 1
 
-#     while left < right:
-#         curr_sum = arr[left] + arr[right]
-#         if curr_sum == target:
-#             return True
-#         elif curr_sum < target:
-#             left += 1
-#         else:
-#             right -= 1
-#     return False
+    while left < right:
+        curr_sum = arr[left] + arr[right]
+        if curr_sum == target:
+            return True
+        elif curr_sum < target:
+            left += 1
+        else:
+            right -= 1
+    return False
 
 
 # arr = [1, 3, 4, 5, 7, 10, 12]
